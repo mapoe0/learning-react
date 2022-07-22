ce1c87036cabf3541e615380e376edc774ebaa11
mapoe0 <pollet.aurelien5@gmail.com>
2022-07-22 14:21:58 +0200

    .

--- src/components/SearchForm/SearchFrom.jsx
+++ src/components/SearchForm/SearchFrom.jsx
@@ -1,10 +1,17 @@
-import React, { useContext } from "react";
+import React, { Component, useContext, useState } from "react";
 import { DataContext } from "../../DataContext";
+import '../SearchForm/SearchForm.css'
 export default function SearchForm() {
-    const {data} = useContext(DataContext)
-
+    const { data } = useContext(DataContext)
+    const [message,setMessage] = useState('')
+    const handleChange = event => {
+        setMessage(event.target.value)
+        console.log(message)
+    }
     return (<div>
-        <input />
+        <div className="block"><p className="test">Commncer</p></div>
+        <input name="field" id="field" onChange={handleChange}/>
+        <button>Send</button>
         <p>Data récup du provider: {data.title}</p>
     </div>)
 }
